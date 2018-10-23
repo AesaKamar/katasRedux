a829ad716ecaeca2087c807855e3e0e605967d1a
AesaKamar <aesa.kamar@gmail.com>
2018-10-23 18:30:44 -0400

    whatever

--- src/day5/Day5Lib.hs
+++ src/day5/Day5Lib.hs
@@ -1,4 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Day5Lib where
+
+import Crypto.Hash (digestToHexByteString, digestFromByteString )
+import qualified Crypto.Hash.MD5 as MD5
 
 
 input = "ugkcyxxp"
+
+
+sss = digestToHexByteString $ MD5.hash "hello"
